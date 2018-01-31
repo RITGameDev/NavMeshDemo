af3a92f35cf4a0d09a439f8493e20f2cf549f8d0
Ben Hoffman <bah8892@g.rit.edu>
2018-01-31 17:54:27 -0500

    Set up a basic script

---
+++ NavMeshDemo/Assets/Scripts/NavMeshtarget.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshtarget : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    
+	// Update is called once per frame
+	void Update ()
+    {
+        CheckInput();
+	}
+
+    private void CheckInput()
+    {
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+                agent.SetDestination(hit.point);
+
+        }
+    }
+}
